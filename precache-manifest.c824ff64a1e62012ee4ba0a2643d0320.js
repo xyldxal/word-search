6abb39666a145861f127eb723802e84f82c288d4
Marxel Abogado <xyldxal@gmail.com>
2025-01-17 03:39:34 +0800

    Updates

---
+++ precache-manifest.c824ff64a1e62012ee4ba0a2643d0320.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13735c108a3a97384bd22fe9ee9d2bae",
+    "url": "/word-search/index.html"
+  },
+  {
+    "revision": "26db76734acced2636c1",
+    "url": "/word-search/static/js/2.26ca192a.chunk.js"
+  },
+  {
+    "revision": "ed20653628b79374665c",
+    "url": "/word-search/static/js/main.1a27cb07.chunk.js"
+  },
+  {
+    "revision": "d7bffd6a3df7e7487e9a",
+    "url": "/word-search/static/js/runtime~main.ff49e425.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/word-search/precache-manifest.a4b13f0058088e5612a8a5ea29899383.js"
+  "/word-search/precache-manifest.c824ff64a1e62012ee4ba0a2643d0320.js"
 );
 
 self.addEventListener('message', (event) => {
