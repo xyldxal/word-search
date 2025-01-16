bb407ae97ac6d6705c6672434e959a7e7b3da53e
Marxel Abogado <xyldxal@gmail.com>
2025-01-17 02:57:53 +0800

    Updates

---
+++ precache-manifest.620a65a8c98d4e265d581600b00ef7b3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5971c6cfac6191cbd786150cb1c2c6e8",
+    "url": "/word-search/index.html"
+  },
+  {
+    "revision": "f63a232ecae87c111fee",
+    "url": "/word-search/static/js/2.7d162562.chunk.js"
+  },
+  {
+    "revision": "973fab897bbbf287e353",
+    "url": "/word-search/static/js/main.2f9afe8f.chunk.js"
+  },
+  {
+    "revision": "d7bffd6a3df7e7487e9a",
+    "url": "/word-search/static/js/runtime~main.ff49e425.js"
+  }
+]);
